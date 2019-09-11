e8ee2a8d44404a2f25f1bf9464d786da56cb26d8
Roman Gruba <roman.gruba@gmail.com>
2019-09-11 17:22:42 +0300

    Updates

---
+++ precache-manifest.e5089ba8b1a935fb411fc4025cd102fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94249af6680bd7dc0afa966eefe1d3c3",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "8b60e096d64d3be520ce",
+    "url": "/goit-react-hw-04-reader/static/css/main.e0f934c7.chunk.css"
+  },
+  {
+    "revision": "4e6266c271f8de1b6a2c",
+    "url": "/goit-react-hw-04-reader/static/js/2.fb775e85.chunk.js"
+  },
+  {
+    "revision": "8b60e096d64d3be520ce",
+    "url": "/goit-react-hw-04-reader/static/js/main.831ba02f.chunk.js"
+  },
+  {
+    "revision": "732b506e4b41a3ced6a5",
+    "url": "/goit-react-hw-04-reader/static/js/runtime~main.3f0b6208.js"
+  }
+]);
