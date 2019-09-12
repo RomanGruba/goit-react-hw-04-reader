4dbe0355fb1073fc0113add8cbf56faee279d70e
Roman Gruba <roman.gruba@gmail.com>
2019-09-13 00:41:39 +0300

    Updates

--- precache-manifest.3ee309607841f922eed1b249411bf8d5.js
+++ precache-manifest.3ee309607841f922eed1b249411bf8d5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3c0db86d96d1d5383fdca9cb0adace71",
+    "revision": "143e20de1199bb3e3dc90a1d4fbf1a11",
     "url": "/goit-react-hw-04-reader/index.html"
   },
   {
-    "revision": "a9d14c2efc4b0c560281",
+    "revision": "e90d5d8b8081041746fd",
     "url": "/goit-react-hw-04-reader/static/css/main.a00cd5dd.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-04-reader/static/js/2.81d3fce2.chunk.js"
   },
   {
-    "revision": "a9d14c2efc4b0c560281",
-    "url": "/goit-react-hw-04-reader/static/js/main.70cba5a1.chunk.js"
+    "revision": "e90d5d8b8081041746fd",
+    "url": "/goit-react-hw-04-reader/static/js/main.48ffdaf0.chunk.js"
   },
   {
     "revision": "732b506e4b41a3ced6a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-04-reader/precache-manifest.8313e79511d140fce1ee26169e260e1e.js"
+  "/goit-react-hw-04-reader/precache-manifest.3ee309607841f922eed1b249411bf8d5.js"
 );
 
 self.addEventListener('message', (event) => {
